20e92e833d70a8cdbd18e086cf2c2da08f6cb42d
Maurits <mcb.westbroek@gmail.com>
2024-10-29 09:02:26 +0000

    Make two platformio.inis and fix Makefiles

--- app/inc/core/system.h
+++ app/inc/core/system.h
@@ -4,7 +4,7 @@
 #include "common-defines.h"
 
 #define CPU_FREQ      (84000000)
-#define SYSTICK_FREQ  (1000)
+#define SYSTICK_FREQ  (100)
 
 void system_setup(void);
 uint64_t system_get_ticks(void);
